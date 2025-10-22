bcc5afbd4cf0a09d914d7d441e64e56aa34970fc
skeen0711 <skeen0711@gmail.com>
2025-10-21 21:24:02 -0500

    Adding basic BST insertion

--- redBlack_test.go
+++ redBlack_test.go
@@ -16,3 +16,31 @@ func TestNode(t *testing.T) {
 		t.Errorf("Expected right node to be 3, got %v", rightValue)
 	}
 }
+
+func TestInsertRedBlackTree(t *testing.T) {
+	root := &Node{1, false, nil, nil}
+	tree := RedBlackTree{root}
+	left := &Node{0, true, nil, nil}
+	right := &Node{2, true, nil, nil}
+	rerr := tree.Insert(left)
+	if rerr != nil {
+		return
+	}
+	lerr := tree.Insert(right)
+	if lerr != nil {
+		return
+	}
+	newRoot := tree.Root
+	if newRoot.Left.Value != 0 {
+		t.Errorf("Expected left node to be 0, got %v", newRoot.Left.Value)
+	}
+	if newRoot.Right.Value != 2 {
+		t.Errorf("Expected right node to be 2, got %v", newRoot.Right.Value)
+	}
+	if newRoot.Left.isRed != true {
+		t.Errorf("Expected left node to be true (red), got %v", newRoot.Left.isRed)
+	}
+	if newRoot.Right.isRed != true {
+		t.Errorf("Expected right node to be true (red), got %v", newRoot.Right.isRed)
+	}
+}
--- redblack.go
+++ redblack.go
@@ -1,5 +1,7 @@
 package redblack
 
+import "errors"
+
 type Node struct {
 	Value int
 	isRed bool
@@ -9,4 +11,35 @@ type Node struct {
 
 type RedBlackTree struct {
 	Root *Node
+}
+
+func (t RedBlackTree) Insert(node *Node) error {
+	// If node is equal, cannot insert
+	// Check if I can continue right or left. If I cannot continue insert on
+	// the correct side of currNode
+	currNode := t.Root
+	for currNode != nil {
+		if node.Value == currNode.Value {
+			return errors.New("cannot insert duplicate value into tree")
+		}
+		if node.Value > currNode.Value {
+			// Check right
+			rightNode := currNode.Right
+			if rightNode == nil {
+				currNode.Right = node
+				return nil
+			} else {
+				currNode = rightNode
+			}
+		} else {
+			leftNode := currNode.Left
+			if leftNode == nil {
+				currNode.Left = node
+				return nil
+			} else {
+				currNode = leftNode
+			}
+		}
+	}
+	return nil
 }
